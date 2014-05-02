07f5453755dae976433a836f41b87cb9ca1f7abc
Mr. Luo <volterra.luo@gmail.com>
2014-05-02 22:44:19 +0800

    adjust urls.py

--- pine/urls.py
+++ pine/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from django.conf import settings
+
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'register.views.home', name='home'),
-    url(r'^register/', include('register.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^', include('register.urls',namespace='regis')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_DEBUG_ROOT}),
+    )
+
+handler404 = 'pine.register.views.handler404'
+handler500 = 'pine.register.views.handler500'
