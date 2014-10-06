4583c37d86d4b4d70348a0ce7f090a2077ca92e9
Volterra <volterra.luo@gmail.com>
2014-10-06 21:27:27 +0800

    modify pine/urls

--- pine/urls.py
+++ pine/urls.py
@@ -21,13 +21,14 @@ router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 
 urlpatterns = patterns('',
+    url(r'^socialauth/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^', include('register.urls',namespace='regis')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^weblog/', include('zinnia.urls')),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^wechat/$', 'wechat.views.index'),
-    url(r'^rest/', include('restframe.urls')),
+    url(r'^rest/', include('restframe.urls')),    
 )
 
 if settings.DEBUG:
